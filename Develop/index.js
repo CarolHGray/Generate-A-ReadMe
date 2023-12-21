84c75bca17d29b0e066c7617fd5b877134fd3428
Carol H Gray <carolhgray@Carols-MacBook-Pro-2.local>
2023-12-21 14:06:17 -0500

    Added input objects

--- Develop/index.js
+++ Develop/index.js
@@ -8,10 +8,73 @@ const generateReadmePage = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 
 const questions = () => {
-    // using inquirer to prompt questions to user 
+
     return inquirer.prompt([
-    {}
-};
+    {
+    
+ // Get input for page title, github, email and description
+
+            type: 'input',
+            name: 'title',
+            message: 'Project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter project name!');
+                    return false; 
+                }
+            }
+        },    
+
+            {
+                type: 'input',
+                name: 'github',
+                message: 'GitHub username?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter GitHub username!');
+                        return false; 
+                    }
+                } 
+            },
+
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description (short) of project.',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter description of project!');
+                        return false; 
+                    }
+                }
+            }, 
+
+            {
+                type: 'input',
+                name: 'email',
+                message: 'email address?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter email address!');
+                        return false; 
+                    }
+                }
+        
+            },   
+        },
+
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
